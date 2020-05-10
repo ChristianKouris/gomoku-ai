f7ba129a1190435f4790f686276fcca531654184
Sicun Gao <scungao@gmail.com>
2020-05-10 14:12:04 -0700

    init

---
+++ ai.py
@@ -0,0 +1,114 @@
+from __future__ import absolute_import, division, print_function
+from math import sqrt, log
+from game import Game, WHITE, BLACK, EMPTY
+import copy
+import time
+import random
+
+class Node:
+    # NOTE: modifying this block is not recommended
+    def __init__(self, state, actions, parent=None):
+        self.state = (state[0], copy.deepcopy(state[1]))
+        self.num_wins = 0 #number of wins at the node
+        self.num_visits = 0 #number of visits of the node
+        self.parent = parent #parent node of the current node
+        self.children = [] #store actions and children nodes in the tree as (action, node) tuples
+        self.untried_actions = copy.deepcopy(actions) #store actions that have not been tried
+
+# NOTE: deterministic_test() requires BUDGET = 1000
+#   You can try higher or lower values to see how the AI's strength changes
+BUDGET = 1000
+
+class AI:
+    # NOTE: modifying this block is not recommended
+    def __init__(self, state):
+        self.simulator = Game()
+        self.simulator.reset(*state) #using * to unpack the state tuple
+        self.root = Node(state, self.simulator.get_actions())
+
+    def mcts_search(self):
+        #TODO: Main MCTS loop
+
+        iters = 0
+        action_ucb_table = {} #store the table of actions and their ucb values
+
+        # TODO: Delete this block ->
+        self.simulator.reset(*self.root.state)
+        for action in self.simulator.get_actions():
+            action_ucb_table[action] = 0
+        return random.choice(self.simulator.get_actions()), action_ucb_table
+        # <- Delete this block
+
+        # TODO: Implement the MCTS Loop
+        while(iters < BUDGET):
+            if ((iters + 1) % 100 == 0):
+                # NOTE: if your terminal driver doesn't support carriage returns
+                #   you can use: print("{}/{}".format(iters + 1, BUDGET))
+                print("\riters/budget: {}/{}".format(iters + 1, BUDGET), end="")
+
+            # TODO: select a node, rollout, and backpropagate
+
+            iters += 1
+        print()
+
+        # TODO: return the best action, and the table of actions and their ucb values
+
+        return action, action_ucb_table
+
+    def select(self, node):
+        # TODO: select a child node
+
+        while node is not None:
+            # NOTE: deterministic_test() requires using c=1 for best_child()
+            break
+        return node
+
+    def expand(self, node):
+        # TODO: add a new child node from an untried action and return this new node
+
+        child_node = None #choose a child node to grow the search tree
+
+        # IMPORTANT: use the following method to fetch the next untried action
+        #   so that the order of action expansion is consistent with the test cases
+        action = node.untried_actions.pop(0)
+
+        # NOTE: Make sure to add the new node to the node.children
+        # NOTE: You may find the following methods useful:
+        #   self.simulator.state()
+        #   self.simulator.get_actions()
+
+        return child_node
+
+    def best_child(self, node, c): 
+        # TODO: determine the best child and action by applying the UCB formula
+
+        best_child_node = None #store the best child node with UCB
+        best_action = None #store the action that leads to the best child
+        action_ucb_table = {} #store the UCB values of each child node (for testing)
+
+        return best_child_node, best_action, action_ucb_table
+
+    def backpropagate(self, node, result):
+        while (node is not None):
+            # TODO: backpropagate the information about winner
+            # IMPORTANT: each node should store the number of wins for the player of its **parent** node
+            break
+
+    def rollout(self, node):
+        # TODO: rollout (called DefaultPolicy in the slides)
+
+        # NOTE: you may find the following methods useful:
+        #   self.simulator.reset(*node.state)
+        #   self.simulator.game_over
+        #   self.simulator.rand_move()
+        #   self.simulator.place(r, c)
+
+        # Determine reward indicator from result of rollout
+        reward = {}
+        if self.simulator.winner == BLACK:
+            reward[BLACK] = 1
+            reward[WHITE] = 0
+        elif self.simulator.winner == WHITE:
+            reward[BLACK] = 0
+            reward[WHITE] = 1
+        return reward
